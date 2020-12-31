e8bfdcefa1191f4277db265cea125824eb953e17
Serg Svet <svetloffsergey@gmail.com>
2020-12-31 07:35:35 +0300

    rebuild dash app

--- app.py
+++ app.py
@@ -1,121 +1,114 @@
-# %%
+%config IPCompleter.greedy=True
+import pandas as pd
+import numpy as np
+import datetime
+import re
+from pandas_datareader import data as web
+import plotly.express as px
+from datetime import datetime as dt
+
+## data NYSE from: ftp://ftp.nasdaqtrader.com/symboldirectory
+df_name = pd.read_csv('~/dash-python-app/datasets/nasdaqlisted.txt', sep = '|', usecols = ['Symbol', 'Security Name'])
+df_name['Company'] = df_name['Security Name'].str.split(' ').str[0]
+df_name = df_name.drop_duplicates(subset = ['Company']).reset_index()
+df_name = df_name.drop(['index'], axis=1)
+df_name['Company'] = df_name['Company'].str.strip(',')
+## make stock company list
+company_dict = []
+symbol_dict = []
+labl_lst = {}
+symb_lst = {}
+for company, symbol in zip(df_name['Company'].values, df_name['Symbol'].values):
+    company_dict.append({'label' : company, 'value' : symbol})
+
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-import plotly.graph_objs as go
-import dlda
+from dash.dependencies import Input, Output
 
-# %%
-data = dlda.data
-start_date = dlda.start_date
-end_date = dlda.end_date
+Time_view = 10
+end_date = dt.today()
+delta = datetime.timedelta(days = 365 * Time_view)
+start_date = end_date - delta
 
-print(start_date, end_date)
-# time_col = dlda.data['date']
-########### Define your variables
-beers=['Chesapeake Stout', 'Snake Dog IPA', 'Imperial Porter', 'Double Dog IPA']
-beers_ide=['CS', 'SDIPA', 'IP', 'DDIPA']
-beers_dict = [{'label' : beer, 'value' : ide} for beer, ide in (zip(beers, beers_ide))]
-
-ibu_values=[35, 60, 85, 75]
-abv_values=[5.4, 7.1, 9.2, 4.3]
-color1='darkred'
-color2='orange'
-mytitle='Beer Comparison'
-mytitle_sales = 'Sales data'
-
-tabtitle='Dash app'
-myheading='Flying Dog Beers'
-label1='IBU'
-label2='ABV'
-githublink='https://github.com/austinlasseter/flying-dog-beers'
-sourceurl='https://www.flyingdog.com/beers/'
-
-########### Set up the chart
-bitterness = go.Bar(
-    x=beers,
-    y=ibu_values,
-    name=label1,
-    marker={'color':color1}
-)
-alcohol = go.Bar(
-    x = beers,
-    y = abv_values,
-    name = label2,
-    marker = {'color':color2}
-)
-
-beer_data = [bitterness, alcohol]
-
-beer_layout = go.Layout(
-    barmode='group',
-    title = mytitle)
-# sales_layout = go.Layout(
-#     barmode='group',
-#     title = mytitle)
-# sales_layout = {'data': [ {'x': data['date'],'y': data['Sales'],'type': 'line'} ],
-#            'layout': go.Layout(xaxis = {"title":"Time"}, yaxis = {"title":"Sales"}) }
-
-beer_fig = go.Figure(data = beer_data, layout = beer_layout)
-# salesdata_fig = go.Figure(data = data, layout = sales_layout)
+rng = pd.date_range(start_date, periods = int(delta.days) // 30, freq = 'm')
+# df_stock = pd.DataFrame({'date': rng, 'Sales': np.random.randint(0, 100, len(rng))}) 
 
 
-########### Initiate the app
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-server = app.server
-app.title=tabtitle
+app = dash.Dash('Hello World')
 
-########### Set up the layout
-app.layout = html.Div(children = [html.H1(myheading), 
-                                  html.Div([dcc.Graph(id = 'example-graph',
-                                                      figure = {'data': [ {'x': data['date'],'y': data['Sales'],'type': 'line'} ],
-                                                                'layout': go.Layout(xaxis = {"title":"Time"}, yaxis = {"title":"Sales"}) }
-                                                     )],
-                                           style = {"display":"inline-block", "width":"35%"}),
-                                  html.Div([dcc.Graph(id='flyingdog',
-                                            figure = beer_fig)],
-                                           style = {"display":"inline-block", "width":"35%"})],
-                      html.A('Code on Github', href=githublink),
-                      html.Br(),
-                      html.A('Data Source', href=sourceurl),
-                                  html.Label('Dropdown'),
-                                  dcc.Dropdown( options = beers_dict,
-                                               #             {'label': 'New York City', 'value': 'NYC'},
-                                               #             {'label': u'Montréal', 'value': 'MTL'},
-                                               #             {'label': 'San Francisco', 'value': 'SF'},
-                                               # value='MTL'
-                                              ),                           
-                                  html.Label('Multi-Select Dropdown'),
-                                  dcc.Dropdown(options = beers_dict,
-                                               value=['MTL', 'SF'],
-                                               multi=True),
-                                  html.Label('Radio Items'), 
-                                  dcc.RadioItems(options = beers_dict, value='MTL'),
-                                  html.Label('Checkboxes'),
-                                  dcc.Checklist(options = beers_dict,
-                                                value = beers_dict),
-                                  html.Label('Text Input'),
-                                  dcc.Input(value='MTL', type='text'),
-                                  html.Label('Slider'),
-                                  dcc.Slider(min=0, max=9,
-                                             marks = {i: 'Label {}'.format(i) if i == 1 else str(i) for i in range(1, 6)},
-                                             value = 0),
-                      style={'columnCount': 2})
+chilgren_top = html.Div(className = 'eight columns',
+                        children = [html.H2('New York Stock Exange'),
+                                    html.H4('build graphics for yuor choise')])
 
-def update_output_div(start_date, end_date):
-    print(start_date, end_date)
-    return {'data': [go.Scatter(x = data[(data['date'] >= start_date) & 
-                                       (data['date'] <= end_date)]['date'], 
-                                y = data[(data['date'] >= start_date) & (data['date'] <= end_date)]['Sales'])],
-            "layout": go.Layout(xaxis = {"title":"Time"}, yaxis = {"title":"Sales"})}
+children_right = html.Div(className = 'pretty_container', 
+                    children = [html.P('Change view period:', className = 'control_label'),
+                                html.Div([dcc.DatePickerRange(id = 'date-picker-range',
+                                                              start_date = end_date - datetime.timedelta(days=365),
+                                                              end_date = end_date,
+                                                              start_date_placeholder_text="Start Period",
+                                                              end_date_placeholder_text="End Period",
+                                                             ),
+                                         ], 
+                                         className = 'pretty_container'
+                                        ),
+                                html.P('Slide left or right:', className = 'control_label'),
+                                html.Div(className = 'pretty_container',
+                                         children = dcc.Slider(id = 'view-period-slider',
+                                                               min = 0,
+                                                               max = 10,
+                                                               step = 1,
+                                                               value = 0)),
+                                html.P('Pick company:', className = 'control_label'),
+                                html.Div(className = 'div-for-dropdown',
+                                         children = [dcc.Dropdown(id = 'my-dropdown',
+                                                                  options=company_dict,
+#                                                                   value = [df_name['Symbol'].values],
+                                                                  value = 'AAPL'
+                                                                 ),
+                                                    ]
+                                        ),
+                               ]
+                   )
+children_left = [html.Div(className = 'pretty_container',
+                          children = [html.Div(children =[html.P('Company'), html.H6(id = 'pick-company')], id = 'used-company'),
+                                      dcc.Graph(id = 'my-graph')])]
 
+app.layout = html.Div(children = [ html.Div(html.H2('Example dashboard use Python with Dash')), html.Div(className = 'row',
+                                            ## top class
+                                            children = [html.Div(className = 'four columns div-user-control',
+                                                    ## right board view - user control
+                                                     children = children_right
+                                                                ),
+                                                        html.Div(className = 'eight columns div-chart-bk-grey',
+                                                    # left board view - graphics view
+                                                    children = children_left
+                                                   )
+                                                       ])
+                                 ])
+@app.callback(Output('my-graph', 'figure'),
+              [Input('my-dropdown', 'value'), Input('date-picker-range', 'start_date')])
+def update_graph(selected_dropdown_value, picked_date):
+    ### df - Yahoo Stock Exn ##########################
+#     print(selected_dropdown_value)
+    df_stock = web.DataReader(selected_dropdown_value,
+                              'yahoo',
+                              picked_date,
+                              dt.now()
+                             )
+#     print(df_stock.head())
+    return {'data': [{'x': df_stock.index,
+                      'y': df_stock.Close}],
+            'layout': {'margin': {'l': 40, 'r': 0, 't': 20, 'b': 30}}
+           }
+@app.callback(Output('pick-company', 'children'),
+              [Input('my-dropdown', 'value')])
+def update_company(selected_dropdown_value):
+        company = list(df_name[df_name['Symbol'] == selected_dropdown_value]['Company'])
 
-# app.layout = html.Div([
-#     dcc.Markdown(children=markdown_text)
-# ])
+        return company
+
+app.css.append_css({'external_url': 'https://codepen.io/chriddyp/pen/bWLwgP.css'})
 
 if __name__ == '__main__':
     app.run_server()
-
-# %%
